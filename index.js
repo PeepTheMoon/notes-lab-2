377918a5d463c3b1d1b624fe738cfd0c62a4881a
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-01 16:53:26 -0700

    adds test for add function with passing result

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 // Instantiates an instance of an "Input" parser module
 // Parses the command line input and returns the command and data
 // Passes the command to the Notes library, which executes the command
+const { parse } = require('./lib/input');
+
+console.log(parse(process.argv));
+
+
+//use parse to create an action from process.argv
+//use valid to check if it's a valid action
+// if valid, execute
+//otherwise, show error 
