64a5cfd310245a853f235b15e26a94ae523e133c
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-02 13:18:58 -0700

    deletes unnecessarry files

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Requires the library files you will be writing (input, notes)
 // Instantiates an instance of an "Input" parser module
 // Parses the command line input and returns the command and data
 // Passes the command to the Notes library, which executes the command
